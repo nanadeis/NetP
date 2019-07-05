f3dee58be6dc38a27311f39b3a2137c187d48f8d
nanadeis <759046363@qq.com>
2019-07-05 12:24:21 +0800

    内含常用头文件

---
+++ common.h
@@ -0,0 +1,15 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include<iostream>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string>
+#include<string.h>
+#include<netinet/in.h>
+
+#endif
